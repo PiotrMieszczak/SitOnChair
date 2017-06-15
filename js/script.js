3ccb07abb03f20f7a0a57d594077788b0c8cb0af
PiotrMieszczak <mieszczak.piotr@gmail.com>
2017-06-15 13:54:24 +0200

    slider done

--- js/script.js
+++ js/script.js
@@ -1,18 +1,75 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
 
-    var submenu = document.querySelector('.submenu');
+        //menu event
+        var submenu = document.querySelector('.submenu');
 
-    submenu.classList.add('hidden');
+        submenu.classList.add('hidden');
 
-    submenu.parentElement.addEventListener('mouseover',function(){
-            submenu.classList.remove('hidden');
-            submenu.classList.add('show_list');
-    });
+        submenu.parentElement.addEventListener('mouseover', function () {
+                submenu.classList.toggle('hidden');
+        });
 
-    submenu.parentElement.addEventListener('mouseout',function(){
-            submenu.classList.remove('show_list');
-            submenu.classList.add('hidden');
-    });
+        submenu.parentElement.addEventListener('mouseout', function () {
+                submenu.classList.toggle('hidden');
+        });
+
+        //gallery event
+        var box_description_background = document.querySelectorAll('.box_description_background');
+        // console.log(box_description_background);
+
+        for (var i = 0; i < box_description_background.length; i++) {
+
+                box_description_background[i].addEventListener('mouseover',function(event){                
+                        this.classList.toggle('hidden');
+                      //  this.firstElementChild.classList.toggle('hidden');
+                        console.log('box');
+                         
+                });
+
+                box_description_background[i].addEventListener('mouseout', function (event) {
+                    // this.classList.toggle('hidden');
+                   //  this.firstElementChild.classList.toggle('hidden');
+                     console.log('out');
+
+                });
+        }
+
+
+        //slider
+        var slider = document.querySelector('.slider');
+        var previous = document.querySelector('.arrow_left');
+        var next = document.querySelector('.arrow_right');
+
+        var sliderLis = slider.querySelectorAll('li');
+
+        var index =0 ;
+
+        next.addEventListener('click', function(){
+
+                sliderLis[index].classList.add('hidden');
+                index++;
+
+                if(index > sliderLis.length-1){ //loop photos
+                        index = 0;
+                }
+
+                sliderLis[index].classList.remove('hidden');
+
+        });
+
+
+        previous.addEventListener('click', function(){
+
+                sliderLis[index].classList.add('hidden');
+                index--;
+
+                if(index < 0 ){ //loop photos
+                        index = sliderLis.length-1
+                }
+
+                sliderLis[index].classList.remove('hidden');
+
+        });
 
 
 });
